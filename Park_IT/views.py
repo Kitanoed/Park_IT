bd4f5838ed25d5d61bf7b669ac5efdc56d18f8c0
Loy <164210663+Kitanoed@users.noreply.github.com>
2025-10-04 20:58:24 +0800

    Database setup and supabase connection, fixed errors

---
+++ Park_IT/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'signIn.html')
+
+
+def signin_student(request: HttpRequest) -> HttpResponse:
+    return render(request, 'signin_student.html')
+
+
+def signin_teacher(request: HttpRequest) -> HttpResponse:
+    return render(request, 'signin_teacher.html')
+
+
+def signin_admin(request: HttpRequest) -> HttpResponse:
+    return render(request, 'signin_admin.html')
+
+
+
